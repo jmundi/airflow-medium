bb25c2008913fb152be9b2da2b083ced2c0aaccd
Jack Mundi <jmundi@maverickquantgroup.com>
2019-08-07 17:18:50 -0700

    No comments

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# docker-airflow
+# Medium Airflow Series
 
 ## Informations
 
--- dags/master.py
+++ dags/master.py
@@ -15,11 +15,7 @@ def good_morning_world():
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": datetime(2019, 8, 7),
-    "email_on_failure": False,
-    "email_on_retry": False,
-    "retries": 1,
-    "retry_delay": timedelta(days=1)
+    "start_date": datetime(2019, 8, 7)
 }
 
 dag = DAG("Medium-AirFlow-Series", default_args=default_args, schedule_interval=timedelta(1))
